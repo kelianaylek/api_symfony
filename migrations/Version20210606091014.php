d26eccf634f9987b2dbc7373e692ff8bdefbe380
kelian <raoultkelian@gmail.com>
2021-06-07 11:56:19 +0200

    ADD - Migration to reset database schema

---
+++ migrations/Version20210606091014.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210606091014 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526CF675F31B');
+        $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8DF675F31B');
+        $this->addSql('ALTER TABLE post_likes DROP CONSTRAINT FK_DED1C292A76ED395');
+        $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526C4B89032C');
+        $this->addSql('ALTER TABLE post_likes DROP CONSTRAINT FK_DED1C2924B89032C');
+        $this->addSql('DROP TABLE app_user');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE post_likes');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+    }
+}
