a77a6761cca7a885f75bcc4efd29d1c357a19569
kelian <raoultkelian@gmail.com>
2021-06-08 11:49:55 +0200

    UPD - migration's name to update database in heroku

--- migrations/Version20210606091011.php
+++ migrations/Version20210606091011.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210606091012 extends AbstractMigration
+final class Version20210606091011 extends AbstractMigration
 {
     public function getDescription(): string
     {
