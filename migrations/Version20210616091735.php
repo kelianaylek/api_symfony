eba1b044a31f3d6fc2de38a2b123d7f837aa821b
kelian <raoultkelian@gmail.com>
2021-06-16 11:18:51 +0200

    UPD - Migrations

--- migrations/Version20210616091735.php
+++ migrations/Version20210616091735.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210610105527 extends AbstractMigration
+final class Version20210616091735 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -39,14 +39,22 @@ final class Version20210610105527 extends AbstractMigration
         $this->addSql('DROP TABLE post_likes');
 
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE poll_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE poll_choice_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE app_user (id INT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_88BDF3E9E7927C74 ON app_user (email)');
         $this->addSql('CREATE TABLE comment (id INT NOT NULL, author_id INT DEFAULT NULL, post_id INT DEFAULT NULL, message TEXT NOT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_9474526CF675F31B ON comment (author_id)');
         $this->addSql('CREATE INDEX IDX_9474526C4B89032C ON comment (post_id)');
         $this->addSql('COMMENT ON COLUMN comment.published_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE "group" (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE group_user (group_id INT NOT NULL, user_id INT NOT NULL, PRIMARY KEY(group_id, user_id))');
+        $this->addSql('CREATE INDEX IDX_A4C98D39FE54D947 ON group_user (group_id)');
+        $this->addSql('CREATE INDEX IDX_A4C98D39A76ED395 ON group_user (user_id)');
+        $this->addSql('CREATE TABLE group_admins (group_id INT NOT NULL, user_id INT NOT NULL, PRIMARY KEY(group_id, user_id))');
+        $this->addSql('CREATE INDEX IDX_7166CDDFFE54D947 ON group_admins (group_id)');
+        $this->addSql('CREATE INDEX IDX_7166CDDFA76ED395 ON group_admins (user_id)');
+        $this->addSql('CREATE TABLE message (id INT NOT NULL, author_id INT NOT NULL, in_group_id INT NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B6BD307FF675F31B ON message (author_id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307FB9ADA51B ON message (in_group_id)');
         $this->addSql('CREATE TABLE poll (id INT NOT NULL, post_id INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_84BCFA454B89032C ON poll (post_id)');
         $this->addSql('CREATE TABLE poll_choice (id INT NOT NULL, poll_id INT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
@@ -62,6 +70,12 @@ final class Version20210610105527 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_DED1C292A76ED395 ON post_likes (user_id)');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CF675F31B FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C4B89032C FOREIGN KEY (post_id) REFERENCES post (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE group_user ADD CONSTRAINT FK_A4C98D39FE54D947 FOREIGN KEY (group_id) REFERENCES "group" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE group_user ADD CONSTRAINT FK_A4C98D39A76ED395 FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE group_admins ADD CONSTRAINT FK_7166CDDFFE54D947 FOREIGN KEY (group_id) REFERENCES "group" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE group_admins ADD CONSTRAINT FK_7166CDDFA76ED395 FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FF675F31B FOREIGN KEY (author_id) REFERENCES app_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FB9ADA51B FOREIGN KEY (in_group_id) REFERENCES "group" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE poll ADD CONSTRAINT FK_84BCFA454B89032C FOREIGN KEY (post_id) REFERENCES post (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE poll_choice ADD CONSTRAINT FK_2DAE19C93C947C0F FOREIGN KEY (poll_id) REFERENCES poll (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE poll_choice_user ADD CONSTRAINT FK_EA6E1E6852514F25 FOREIGN KEY (poll_choice_id) REFERENCES poll_choice (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -76,18 +90,26 @@ final class Version20210610105527 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526CF675F31B');
+        $this->addSql('ALTER TABLE group_user DROP CONSTRAINT FK_A4C98D39A76ED395');
+        $this->addSql('ALTER TABLE group_admins DROP CONSTRAINT FK_7166CDDFA76ED395');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FF675F31B');
         $this->addSql('ALTER TABLE poll_choice_user DROP CONSTRAINT FK_EA6E1E68A76ED395');
         $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8DF675F31B');
         $this->addSql('ALTER TABLE post_likes DROP CONSTRAINT FK_DED1C292A76ED395');
+        $this->addSql('ALTER TABLE group_user DROP CONSTRAINT FK_A4C98D39FE54D947');
+        $this->addSql('ALTER TABLE group_admins DROP CONSTRAINT FK_7166CDDFFE54D947');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FB9ADA51B');
         $this->addSql('ALTER TABLE poll_choice DROP CONSTRAINT FK_2DAE19C93C947C0F');
         $this->addSql('ALTER TABLE poll_choice_user DROP CONSTRAINT FK_EA6E1E6852514F25');
         $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526C4B89032C');
         $this->addSql('ALTER TABLE poll DROP CONSTRAINT FK_84BCFA454B89032C');
         $this->addSql('ALTER TABLE post_likes DROP CONSTRAINT FK_DED1C2924B89032C');
-        $this->addSql('DROP SEQUENCE poll_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE poll_choice_id_seq CASCADE');
         $this->addSql('DROP TABLE app_user');
         $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE "group"');
+        $this->addSql('DROP TABLE group_user');
+        $this->addSql('DROP TABLE group_admins');
+        $this->addSql('DROP TABLE message');
         $this->addSql('DROP TABLE poll');
         $this->addSql('DROP TABLE poll_choice');
         $this->addSql('DROP TABLE poll_choice_user');
