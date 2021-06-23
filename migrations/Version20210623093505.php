1e22bfe5a79ca34cd2f59243796dfd048636b830
kelian <raoultkelian@gmail.com>
2021-06-23 11:37:04 +0200

    ADD - New migration

--- migrations/Version20210623093505.php
+++ migrations/Version20210623093505.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210616092933 extends AbstractMigration
+final class Version20210623093505 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,6 +19,7 @@ final class Version20210616092933 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526CF675F31B');
         $this->addSql('ALTER TABLE event DROP CONSTRAINT FK_3BAE0AA77E3C61F9');
         $this->addSql('ALTER TABLE event_user DROP CONSTRAINT FK_92589AE2A76ED395');
@@ -112,44 +113,21 @@ final class Version20210616092933 extends AbstractMigration
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DF675F31B FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE post_likes ADD CONSTRAINT FK_DED1C2924B89032C FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE post_likes ADD CONSTRAINT FK_DED1C292A76ED395 FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->addSql('CREATE TABLE event (id INT NOT NULL, owner_id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA77E3C61F9 ON event (owner_id)');
+        $this->addSql('CREATE TABLE event_user (event_id INT NOT NULL, user_id INT NOT NULL, PRIMARY KEY(event_id, user_id))');
+        $this->addSql('CREATE INDEX IDX_92589AE271F7E88B ON event_user (event_id)');
+        $this->addSql('CREATE INDEX IDX_92589AE2A76ED395 ON event_user (user_id)');
+
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA77E3C61F9 FOREIGN KEY (owner_id) REFERENCES app_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE271F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE2A76ED395 FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526CF675F31B');
-        $this->addSql('ALTER TABLE group_user DROP CONSTRAINT FK_A4C98D39A76ED395');
-        $this->addSql('ALTER TABLE group_admins DROP CONSTRAINT FK_7166CDDFA76ED395');
-        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FF675F31B');
-        $this->addSql('ALTER TABLE poll_choice_user DROP CONSTRAINT FK_EA6E1E68A76ED395');
-        $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8DF675F31B');
-        $this->addSql('ALTER TABLE post_likes DROP CONSTRAINT FK_DED1C292A76ED395');
-        $this->addSql('ALTER TABLE group_user DROP CONSTRAINT FK_A4C98D39FE54D947');
-        $this->addSql('ALTER TABLE group_admins DROP CONSTRAINT FK_7166CDDFFE54D947');
-        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FB9ADA51B');
-        $this->addSql('ALTER TABLE poll_choice DROP CONSTRAINT FK_2DAE19C93C947C0F');
-        $this->addSql('ALTER TABLE poll_choice_user DROP CONSTRAINT FK_EA6E1E6852514F25');
-        $this->addSql('ALTER TABLE comment DROP CONSTRAINT FK_9474526C4B89032C');
-        $this->addSql('ALTER TABLE poll DROP CONSTRAINT FK_84BCFA454B89032C');
-        $this->addSql('ALTER TABLE post_likes DROP CONSTRAINT FK_DED1C2924B89032C');
-        $this->addSql('DROP SEQUENCE app_user_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE comment_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE "group_id_seq" CASCADE');
-        $this->addSql('DROP SEQUENCE message_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE poll_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE poll_choice_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE post_id_seq CASCADE');
-        $this->addSql('DROP TABLE app_user');
-        $this->addSql('DROP TABLE comment');
-        $this->addSql('DROP TABLE "group"');
-        $this->addSql('DROP TABLE group_user');
-        $this->addSql('DROP TABLE group_admins');
-        $this->addSql('DROP TABLE message');
-        $this->addSql('DROP TABLE poll');
-        $this->addSql('DROP TABLE poll_choice');
-        $this->addSql('DROP TABLE poll_choice_user');
-        $this->addSql('DROP TABLE post');
-        $this->addSql('DROP TABLE post_likes');
     }
 }
