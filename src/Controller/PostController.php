c1f88e9695632d6a084a9ecb15f06a222d8c9ee7
kelian <raoultkelian@gmail.com>
2021-06-29 11:49:25 +0200

    ADD - swagger annotations in post controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -17,10 +17,11 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
 
 /**
- * Class PostController
- * @package App\Controller
  * @Route("/api/posts")
  */
 class PostController extends BaseController
@@ -44,9 +45,20 @@ class PostController extends BaseController
     }
 
     /**
-     * @return JsonResponse
-     * @Route(name="api_posts_collection_get", methods={"GET"})
+     * List all posts.
      *
+     * This is the list of all posts.
+     *
+     * @Route(name="api_posts_collection_get", methods={"GET"})
+     * @SWG\Response(
+     *     response=Response::HTTP_OK,
+     *     description="Returns all posts",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"post", "user", "comment", "likers", "poll", "poll_posts", "poll_choices"}))
+     *     )
+     * )
+     * @SWG\Tag(name="posts")
      */
     public function collection(): JsonResponse
     {
@@ -56,9 +68,24 @@ class PostController extends BaseController
     }
 
     /**
+     * Return the specified post.
+     *
+     * This call return a specific post.
+     *
      * @Route("/{id}", name="api_posts_item_get", methods={"GET"})
-     * @param Post $post
-     * @return JsonResponse
+     * @SWG\Response(
+     *     response=Response::HTTP_OK,
+     *     description="Returns a specific post",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"post", "user", "comment", "likers", "poll", "poll_posts", "poll_choices"}))
+     *     )
+     * )
+     *     @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="Post not found"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
     public function item(Post $post): JsonResponse
     {
@@ -66,10 +93,38 @@ class PostController extends BaseController
     }
 
     /**
+     * Create a new post.
+     * This call create a new post.
      * @Route("/{userId}", name="api_posts_collection_post", methods={"POST"})
-     * @param Request $request
-     * @param int $userId
-     * @return JsonResponse
+     * @SWG\Parameter(
+     *          name="post data",
+     *          in="body",
+     *          type="json",
+     *          description="Post data",
+     *          required=true,
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(property="content", type="string"),
+     *              @SWG\Property(property="image", type="string"),
+     *          )
+     *     ),
+     * @SWG\Response(
+     *     response=Response::HTTP_CREATED,
+     *     description="Returns a specific user",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"user", "posts", "post"}))
+     *     )
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_BAD_REQUEST,
+     *         description="A problem occured with a field"
+     *     ),
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="This user does not exists"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
     public function post(Request $request, int $userId): JsonResponse
     {
@@ -89,7 +144,38 @@ class PostController extends BaseController
     }
 
     /**
+     * Update a post.
+     * This call update a post.
      * @Route("/{id}", name="api_posts_item_put", methods={"PUT"})
+     * @SWG\Parameter(
+     *          name="post data",
+     *          in="body",
+     *          type="json",
+     *          description="Post data",
+     *          required=true,
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(property="content", type="string"),
+     *              @SWG\Property(property="image", type="string"),
+     *          )
+     *     ),
+     * @SWG\Response(
+     *     response=Response::HTTP_CREATED,
+     *     description="Returns a specific post after updated",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"user", "posts", "post"}))
+     *     )
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_BAD_REQUEST,
+     *         description="A problem occured with a field"
+     *     ),
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="This post does not exists"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
     public function put(Post $post, Request $request): JsonResponse
     {
@@ -108,9 +194,19 @@ class PostController extends BaseController
     }
 
     /**
+     * Delete a specified post.
+     *
+     * This call delete a specific post.
      * @Route("/{id}", name="api_posts_item_delete", methods={"DELETE"})
-     * @param Post $post
-     * @return JsonResponse
+     * @SWG\Response(
+     *     response=Response::HTTP_NO_CONTENT,
+     *     description="Post deleted successfully",
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="Post not found"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
     public function delete(Post $post): JsonResponse
     {
@@ -121,21 +217,31 @@ class PostController extends BaseController
     }
 
     /**
-     * @Route("/addLike/{id}/{userId}", name="api_posts_item_add_like", methods={"PUT"})
+     * Add a like to post.
+     * This call update a post by adding a like.
+     * @Route("/addLike/{id}", name="api_posts_item_add_like", methods={"PUT"})
+     * @SWG\Response(
+     *     response=Response::HTTP_CREATED,
+     *     description="Returns a specific post after updated with a like",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"post", "user", "comment","likers"}))
+     *     )
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="This post does not exists"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
-    public function addLike($id, $userId): JsonResponse
+    public function addLike($id): JsonResponse
     {
         $userLoggedIn = $this->getUser();
-        $userLoggedInId = $userLoggedIn->getId();
-        if ($userLoggedInId != $userId) {
-            throw $this->createAccessDeniedException('Vous ne pouvez pas liker ce post avec ce compte.');
-        }
         $post = $this->entityManager->getRepository(Post::class)->find($id);
         if($post === null){
             return $this->json(null, Response::HTTP_NOT_FOUND);
         }
-        $user = $this->entityManager->getRepository(User::class)->find($userId);
-        $post->likeBy($user);
+        $post->likeBy($userLoggedIn);
         $this->entityManager->persist($post);
         $this->entityManager->flush();
 
@@ -143,21 +249,31 @@ class PostController extends BaseController
     }
 
     /**
-     * @Route("/removeLike/{id}/{userId}", name="api_posts_item_remove_like", methods={"PUT"})
+     * Remove a like from post.
+     * This call update a post by removing a like.
+     * @Route("/removeLike/{id}", name="api_posts_item_remove_like", methods={"PUT"})
+     * @SWG\Response(
+     *     response=Response::HTTP_CREATED,
+     *     description="Returns a specific post after updated with a like",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"post", "user", "comment","likers"}))
+     *     )
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="This post does not exists"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
-    public function removeLike($id, $userId): JsonResponse
+    public function removeLike($id): JsonResponse
     {
         $userLoggedIn = $this->getUser();
-        $userLoggedInId = $userLoggedIn->getId();
-        if ($userLoggedInId != $userId) {
-            throw $this->createAccessDeniedException('Vous ne pouvez pas liker ce post avec ce compte.');
-        }
         $post = $this->entityManager->getRepository(Post::class)->find($id);
         if($post === null){
             return $this->json(null, Response::HTTP_NOT_FOUND);
         }
-        $user = $this->entityManager->getRepository(User::class)->find($userId);
-        $post->dislikeBy($user);
+        $post->dislikeBy($userLoggedIn);
         $this->entityManager->persist($post);
         $this->entityManager->flush();
 
@@ -165,7 +281,26 @@ class PostController extends BaseController
     }
 
     /**
+     * Add event to post.
+     * This call update a post by adding an event.
      * @Route("/addEvent/{id}/{eventId}", name="api_posts_item_add_event", methods={"PUT"})
+     * @SWG\Response(
+     *     response=Response::HTTP_OK,
+     *     description="Returns a specific post after updated with an event",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"post", "user", "post_event", "event"}))
+     *     )
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="This post or event does not exists"
+     *     ),
+     * @SWG\Response(
+     *         response=Response::HTTP_FORBIDDEN,
+     *         description="You don't have the authorization do to this"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
     public function addEvent(Post $post, $eventId): JsonResponse
     {
@@ -197,7 +332,26 @@ class PostController extends BaseController
     }
 
     /**
+     * Remove event to post.
+     * This call update a post by removing an event.
      * @Route("/removeEvent/{id}", name="api_posts_item_remove_event", methods={"PUT"})
+     * @SWG\Response(
+     *     response=Response::HTTP_OK,
+     *     description="Returns a specific post after updated with an event",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Post::class, groups={"post", "user", "post_event", "event"}))
+     *     )
+     * )
+     * @SWG\Response(
+     *         response=Response::HTTP_NOT_FOUND,
+     *         description="This post does not exists"
+     *     ),
+     * @SWG\Response(
+     *         response=Response::HTTP_FORBIDDEN,
+     *         description="You don't have the authorization do to this"
+     *     ),
+     * @SWG\Tag(name="posts")
      */
     public function removeEvent(Post $post): JsonResponse
     {
